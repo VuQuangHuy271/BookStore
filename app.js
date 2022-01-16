d5ad3f16d72029c8de6abf4c9df3d8d6cc3cbfb6
splace271 <94423485+splace271@users.noreply.github.com>
2022-01-16 20:45:45 +0700

    ...

--- app.js
+++ app.js
@@ -18,6 +18,40 @@ app.get('/loginAdmin', async (req,res)=>{
 app.get('/register', async (req,res)=>{
     res.render('register')
 }) 
+app.post('/register', async (req,res)=>{
+    const nameInput = req.body.txtName
+    const phoneInput = req.body.txtPhone
+    const emailInput = req.body.txtEmail
+    const genderInput = req.body.txtGender
+    const cityInput = req.body.txtCity
+    const countryInput = req.body.txtCountry
+    const passwordInput = req.body.txtPassword
+    const confirmInput = req.body.txtConfirm
+    // if(isNaN(phoneInput)==true){
+    //     const errorMessage = "Gia phai la so!"
+    //     const oldValues = {name: nameInput, phone: phoneInput, email: emailInput, gender: genderInput, city: cityInput, country: countryInput, password: passwordInput} 
+    //     res.render('register', {error: errorMessage , oldValues:oldValues})
+    //     return;
+    // }
+    if(phoneInput.length >= 12 || phoneInput.length < 9)
+    {
+        const errorDes="do dai cua chuoi tu 9 - 12";
+        const oldValues = {name: nameInput, phone: phoneInput, email: emailInput, gender: genderInput, city: cityInput, country: countryInput, password: passwordInput} 
+        res.render('register', {errorD: errorDes,  oldValues:oldValues})
+        return;
+    }
+    if(passwordInput != confirmInput)
+    {
+        const errorConfirm = "Sai mật khẩu"
+        const oldValues = {name: nameInput, phone: phoneInput, email: emailInput, gender: genderInput, city: cityInput, country: countryInput, password: passwordInput}
+        res.render('register', {errorCon: errorConfirm,  oldValues:oldValues}) 
+        return;
+    }
+    const newC =  {name: nameInput, phone: phoneInput, email: emailInput, gender: genderInput, city: cityInput, country: countryInput, password: passwordInput}
+    const collectionName = "AccountOfCustomer"
+    insertObject(collectionName, newC)
+    res.redirect('login')
+}) 
 app.get('/', async (req,res)=>{
     const results = await getAllDocuments("Products")
     res.render('index', {products : results})
